964ae913dd10d10f844e3805cca001b43dbe90cd
Tkyou806 <162584559+Tkyou806@users.noreply.github.com>
2024-06-10 01:45:50 +0900

    Update Order.java

--- Order.java
+++ Order.java
@@ -3,26 +3,36 @@ package JavaTeamProject;
 
 public class Order {
     private String name;//상품명
-    private double price;//가격
-    private double num;//주문량
+    private int price;//가격
+    private int num;//주문량
     private int where;//장소
 
 
-    public Order(String name, double price, double num, int where){
+    public Order(String name, int price, int num, int where){
         this.name = name;
         this.price = price;
         this.num = num;
         this.where = where;
     }
 
-    public double getNum(){//주문 시 재고량 관리를 위해
+    public int getNum(){//주문 시 재고량 관리를 위해
         return num;
+    }
+    public void setNum(int num){//주문 시 재고량 관리를 위해
+        this.num = num;
     }
 
     public String toString(){//주문 시 재고량 관리를 위해
-        return name+ "\t\t\t\t" + price + "\t\t\t" + num + "\t\t\t" +  where;
+        return name+ " " + price + " " + num + " " +  where;
 
     }
+
+    public String toStringForClient(){//고객을 위해
+        return name+ "\t\t\t\t" + price + "\t\t\t" + num;
+
+    }
+
+
     public Order getOrder(){//
         return new Order(name, price, num, where);
     }
