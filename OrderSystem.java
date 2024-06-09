405711f44f18ec2849e5b4048d93e7404b6feb6a
Tkyou806 <162584559+Tkyou806@users.noreply.github.com>
2024-06-10 01:48:29 +0900

    Update OrderSystem.java

--- OrderSystem.java
+++ OrderSystem.java
@@ -1,11 +1,31 @@
 package JavaTeamProject;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class OrderSystem {
     HashMap<String, Product> map;
     public OrderSystem() {
         map = new HashMap<>();
+        String file = "product.txt";
+        Scanner inputStream = null;
+        try {
+            inputStream = new Scanner(new File(file));
+        } catch (FileNotFoundException e) {
+            System.out.println("Fail to open the file!");
+            System.exit(0);
+        }
+        //파일에 줄이 끝날 때까지 받아드리면서 데이터화하자.
+        while (inputStream.hasNextLine()) {
+            String line = inputStream.nextLine();
+            String[] data = line.split(" ");
+            //Product에 넣자.
+            Product item = new Product(data[0], Integer.parseInt(data[1]), Integer.parseInt(data[2]), Integer.parseInt(data[3]));
+            //HashMap w에 넣음
+            map.put(data[0], item);
+        }
     }
     void addHashMap(String name, Product p) {
         map.put(name, p);
@@ -13,5 +33,10 @@ public class OrderSystem {
     void printHashMap() {
         for (String key : map.keySet())
             System.out.println("Name: " + key + map.get(key).toString());
+    }
+
+    void printHashMapforClient() {
+        for (String key : map.keySet())
+            System.out.println("Name: " + key + map.get(key).toStringForClient());
     }
 }
