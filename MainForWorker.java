8bdba0293f32eb98a3e2570bb3fd40b035d4ffad
min06150315 <min06150315@gmail.com>
2024-06-12 00:33:13 +0900

    Update MainForWorker.java

--- MainForWorker.java
+++ MainForWorker.java
@@ -2,7 +2,7 @@ package JavaTeamProject;
 
 import java.util.Scanner;
 
-public class MainForWorkrer {
+public class MainForWorker {
     public static void main(String[] args) {
         Worker s = new Worker();
         Scanner k = new Scanner(System.in);
