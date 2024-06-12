42fe63afe9d4b3e1ada9601866604a24a4653881
min06150315 <min06150315@gmail.com>
2024-06-13 00:19:02 +0900

    Update Worker.java

--- Worker.java
+++ Worker.java
@@ -1,15 +1,13 @@
 package JavaTeamProject;
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class Worker {
     private HashMap<String, Order> w = new HashMap<>();
-    private boolean ing;
 
     public Worker(){// 이 class의 object를 만든 순간 데이터를 가지고 있다.
-        String file = "order1.txt";
+        String file = "./src/JavaTeamProject/textfiles/order.txt";
         Scanner inputStream = null;
         try{
             inputStream = new Scanner(new File(file));
@@ -25,12 +23,10 @@ public class Worker {
             Order item = new Order(data[0], Integer.parseInt(data[1]), Integer.parseInt(data[2]),Integer.parseInt(data[3]));
             //HashMap w에 넣음
             w.put(data[0],item);
-
-
         }
         //여기 까지 오면 파일에 있는 건 다 받아드린 것
     }
-    public int clearWork(String name) {//일 했다면 list에서 지워주기
+    public int ClearWork(String name) {//일 했다면 list에서 지워주기
         if (w.isEmpty()) {//처음 부터 할 일이 없었다면
             System.out.println("All Cleared.");
             return 1;
@@ -51,7 +47,6 @@ public class Worker {
             }
             if(i == 1){
                 System.out.println("Not Order item.");
-
             }
         }
         return 0;
@@ -59,37 +54,33 @@ public class Worker {
 
     public void ShowWorkList() {
         int i = 1;
-        System.out.println("test");
-        System.out.println("i)   name        price     Order number     where ");
-        for (String item : w.keySet()) {
+        System.out.println("   Name      \tPrice\tOrder number \tLocation ");
+        for (String item : w.keySet())
             System.out.println(i++ + ") " +w.get(item).toString());
-        }
-
-
     }
-    public void ShowWhere(int num){
+    public void ShowLocation(int num) {
         if(num <= 6) {
-            System.out.println(" --floor 1-- ");
+            System.out.println(" --Floor 1-- ");
             System.out.println(" --------------------------------------- ");
             System.out.println("|       |       |       |               |");
             System.out.println("|       |       |       |               |");
             System.out.println("|   1   |   2   |   3   |               |");
             System.out.println("|       |       |       |               |");
             System.out.println("|       |       |       |               |");
-            System.out.println(" ------------------------                ");
+            System.out.println("| ------------------------              |");
             System.out.println("|                                       |");
             System.out.println("|e                                      |");
-            System.out.println(" x                ---------------------- ");
+            System.out.println("|x                ----------------------|");
             System.out.println("|i                |      |       |      |");
             System.out.println("|t                |      |       |      |");
             System.out.println("|                 |      |       |      |");
             System.out.println("|                 |   4  |   5   |   6  |");
             System.out.println("|                 |      |       |      |");
             System.out.println("|                 |      |       |      |");
-            System.out.println(" ----------------------------------------");
+            System.out.println(" --------------------------------------- ");
 
-        }else{
-            System.out.println(" --floor 2-- ");
+        } else {
+            System.out.println(" --Floor 2-- ");
             System.out.println(" --------------------------------------- ");
             System.out.println("|       |       |       |       |       |");
             System.out.println("|       |       |       |       |       |");
@@ -97,17 +88,39 @@ public class Worker {
             System.out.println("|       |   7   |   8   |   9   |  10   |");
             System.out.println("|       |       |       |       |       |");
             System.out.println("|       |       |       |       |       |");
-            System.out.println("        -------------------------------- ");
+            System.out.println("|       --------------------------------|");
             System.out.println("|                                       |");
             System.out.println("|                                       |");
-            System.out.println("                  ---------------------- ");
+            System.out.println("|                 ----------------------|");
             System.out.println("|                 |      |       |      |");
             System.out.println("|                 |      |       |      |");
             System.out.println("|                 |      |       |      |");
             System.out.println("|                 |  11  |  12   |  13  |");
             System.out.println("|                 |      |       |      |");
             System.out.println("|      exit       |      |       |      |");
-            System.out.println(" ----------------------------------------");
+            System.out.println(" --------------------------------------- ");
+        }
+    }
+    public void PrintStateEndOrNot() {
+        String fileName = "./src/JavaTeamProject/textfiles/finish.txt";
+
+        try (FileWriter fileWriter = new FileWriter(fileName);
+             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
+                bufferedWriter.write("true");
+                System.out.println("Write Complete.");
+        } catch (IOException e) {
+            System.err.println("Error");
+        }
+    }
+    void SendMessage() {
+        String fileName = "./src/JavaTeamProject/textfiles/message.txt";
+
+        try (FileWriter fileWriter = new FileWriter(fileName);
+             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
+                bufferedWriter.write("Your order has been shipped.");
+                System.out.println("Write Complete.");
+        } catch (IOException e) {
+            System.err.println("Error");
         }
     }
 }
