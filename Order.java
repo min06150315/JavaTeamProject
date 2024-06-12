f40c69ddbc61eae5ced261a752e1c9699d867953
min06150315 <min06150315@gmail.com>
2024-06-13 00:18:32 +0900

    Update Order.java

--- Order.java
+++ Order.java
@@ -1,45 +1,38 @@
 package JavaTeamProject;
 
-
 public class Order {
     private String name;//상품명
     private int price;//가격
     private int num;//주문량
-    private int where;//장소
-
-
-    public Order(String name, int price, int num, int where){
+    private int location;//장소
+    public Order(String name, int price, int num, int location){
         this.name = name;
         this.price = price;
         this.num = num;
-        this.where = where;
+        this.location = location;
     }
-
+    public String getName() {
+        return name;
+    }
+    public int getPrice() {
+        return price;
+    }
     public int getNum(){//주문 시 재고량 관리를 위해
         return num;
     }
     public void setNum(int num){//주문 시 재고량 관리를 위해
         this.num = num;
     }
-
     public String toString(){//주문 시 재고량 관리를 위해
-        return name+ " " + price + " " + num + " " +  where;
-
+        return String.format("%-11s\t%-5d\t%-12d\t%-8d", name, price, num, location);
     }
-
     public String toStringForClient(){//고객을 위해
-        return name+ "\t\t\t\t" + price + "\t\t\t" + num;
-
+        return String.format("%-11s\t%-5d\t%-12d", name, price, num);
     }
-
-
     public Order getOrder(){//
-        return new Order(name, price, num, where);
+        return new Order(name, price, num, location);
     }
-
-    public int getWhere(){//
-        return where;
+    public int getLocation(){//
+        return location;
     }
-
-
 }
